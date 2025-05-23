6d4f90026e805a42decacd8ca737312fcaf988bd
Eric Ritter <ejritter87@gmail.com>
2025-05-23 19:22:52 -0400

    correcting namespace scope.

--- WeightAndSee/Pages/GeneralPopupPage.cs
+++ WeightAndSee/Pages/GeneralPopupPage.cs
@@ -1,57 +1,56 @@
-﻿namespace WeightAndSee.Pages
+﻿namespace WeightAndSee.Pages;
+
+public class GeneralPopupPage : BasePopupPage<GeneralPopupViewModel>
 {
-    public class GeneralPopupPage : BasePopupPage<GeneralPopupViewModel>
+    public GeneralPopupPage(GeneralPopupViewModel vm) : base(vm)
     {
-        public GeneralPopupPage(GeneralPopupViewModel vm) : base(vm)
-        {
-        }
+    }
 
-        protected override void Build()
-        {
-            this.Bind(CanBeDismissedByTappingOutsideOfPopupProperty, getter: (GeneralPopupViewModel vm) => vm.IsDismissable);
+    protected override void Build()
+    {
+        this.Bind(CanBeDismissedByTappingOutsideOfPopupProperty, getter: (GeneralPopupViewModel vm) => vm.IsDismissable);
 
-            Content = new VerticalStackLayout()
+        Content = new VerticalStackLayout()
+        {
+            Spacing = 3,
+            Padding = 3,
+            Margin = 3,
+
+            Children =
             {
-                Spacing = 3,
-                Padding = 3,
-                Margin = 3,
+                new Label()
+                    .Bind(Label.TextProperty, getter:(GeneralPopupViewModel vm) => vm.Title)
+                    .Center()
+                    .Top(),
 
-                Children =
+                new Label()
+                     .Bind(Label.TextProperty, getter:(GeneralPopupViewModel vm) => vm.Message)
+                     .Center()
+                     .CenterHorizontal(),
+                new HorizontalStackLayout()
                 {
-                    new Label()
-                        .Bind(Label.TextProperty, getter:(GeneralPopupViewModel vm) => vm.Title)
-                        .Center()
-                        .Top(),
-
-                    new Label()
-                         .Bind(Label.TextProperty, getter:(GeneralPopupViewModel vm) => vm.Message)
-                         .Center()
-                         .CenterHorizontal(),
-                    new HorizontalStackLayout()
+                    Spacing = 3,
+                    Padding = 3,
+                    Margin = 3,
+                    Children =
                     {
-                        Spacing = 3,
-                        Padding = 3,
-                        Margin = 3,
-                        Children =
-                        {
 
-                            new  Button()
-                                .Text("OK")
-                                .Bind(Button.CommandProperty, getter: (GeneralPopupViewModel vm) => vm.OkayClickedCommand)
-                                .Center()
-                                .Bottom(),
+                        new  Button()
+                            .Text("OK")
+                            .Bind(Button.CommandProperty, getter: (GeneralPopupViewModel vm) => vm.OkayClickedCommand)
+                            .Center()
+                            .Bottom(),
 
-                            new Button()
-                                .Text("Cancel")
-                                .Bind(Button.CommandProperty, getter:(GeneralPopupViewModel vm) => vm.CancelClickedCommand)
-                                .Center()
-                                .Bottom()
-                        }
-                    }.Center()
+                        new Button()
+                            .Text("Cancel")
+                            .Bind(Button.CommandProperty, getter:(GeneralPopupViewModel vm) => vm.CancelClickedCommand)
+                            .Center()
+                            .Bottom()
+                    }
+                }.Center()
 
 
-                }
-            };
-        }
+            }
+        };
     }
 }
